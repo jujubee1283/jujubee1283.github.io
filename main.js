2ab937dc4ff1b1653101205ba905a12fb55ac920
ju7915 <ju7915@att.com>
2015-10-29 19:16:11 -0700

    test get

--- main.js
+++ main.js
@@ -1,15 +1,20 @@
 console.log("hello");
 
-var apiUrl = 'https://api.twitch.tv/kraken/search/streams?q=starcraft';
 
-var xhr = new XMLHttpRequest();
-xhr.open('GET', encodeURI(apiUrl));
-xhr.onload = function() {
-    if (xhr.status === 200) {
-        console.log(xhr.responseText);
-    }
-    else {
-        console.log('Request failed.  Returned status of ' + xhr.status);
-    }
-};
-xhr.send();
+function search(query) {
+	var apiUrl = 'https://api.twitch.tv/kraken/search/streams?q=';
+
+	// use xhr to get the data
+	var xhr = new XMLHttpRequest();
+	xhr.open('GET', encodeURI(apiUrl+query));
+	xhr.onload = function() {
+	    if (xhr.status === 200) {
+	        console.log(xhr.responseText);
+	    } else {
+	        console.log('Request failed.  Returned status of ' + xhr.status);
+	    }
+	};
+	xhr.send();
+}
+
+search('hello');
