5f7ad4d54e19b667e5be6a54f3095f9f59461f7d
ju7915 <ju7915@att.com>
2015-10-29 22:56:39 -0700

    making progress

--- main.js
+++ main.js
@@ -1,20 +1,53 @@
 console.log("hello");
 
+var searchResults;
 
-function search(query) {
+function search() {
 	var apiUrl = 'https://api.twitch.tv/kraken/search/streams?q=';
+	var query = document.getElementById('query').value;
 
 	// use xhr to get the data
 	var xhr = new XMLHttpRequest();
 	xhr.open('GET', encodeURI(apiUrl+query));
 	xhr.onload = function() {
 	    if (xhr.status === 200) {
-	        console.log(xhr.responseText);
+	    	searchResults = JSON.parse(xhr.responseText).streams;
+	        console.log(JSON.parse(xhr.responseText).streams);
+	        populateResults();
 	    } else {
-	        console.log('Request failed.  Returned status of ' + xhr.status);
+	        console.log('Failed! Status: ' + xhr.status);
 	    }
 	};
 	xhr.send();
 }
 
-search('hello');
+function populateResults() {
+	//add results count
+	var totalNode = document.getElementById('total-results');
+	totalNode.innerText = 'Total results: ' + searchResults.length;
+
+	var resultsContainer = document.getElementById('results');
+	for (var i = 0; i < searchResults.length; i ++) {
+		var imageSrc = searchResults[i].preview.small;
+
+		var current = searchResults[i].channel;
+		var streamName = current.display_name;
+		var gameName = current.game;
+		var viewCount = current.views;
+		var description = current.status;
+		var resultHtml =
+			'<div class="result">' +
+				'<img class="image" src="' + imageSrc + '"></img>' +
+				'<div class="details">' +
+					'<div class="stream-name">' + streamName + '</div>' +
+					'<div class="game-and-views">' + gameName + ' - ' + viewCount + ' viewers</div>' +
+					'<div class="stream-description">' + description + '</div>' +
+				'</div>' +
+			'</div>';
+
+		var resultNode = document.createElement('div');
+		resultNode.innerHTML = resultHtml;
+		resultNode.className = 'result';
+		resultsContainer.appendChild(resultNode);
+	}
+}
