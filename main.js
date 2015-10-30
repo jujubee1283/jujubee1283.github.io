c89730297477d9fdea637a821017f2f32a29ae6d
ju7915 <ju7915@att.com>
2015-10-29 19:11:03 -0700

    add test

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
 console.log("hello");
+
+var apiUrl = 'https://api.twitch.tv/kraken/search/streams?q=starcraft';
+
+var xhr = new XMLHttpRequest();
+xhr.open('GET', encodeURI(apiUrl));
+xhr.onload = function() {
+    if (xhr.status === 200) {
+        console.log(xhr.responseText);
+    }
+    else {
+        console.log('Request failed.  Returned status of ' + xhr.status);
+    }
+};
+xhr.send();
